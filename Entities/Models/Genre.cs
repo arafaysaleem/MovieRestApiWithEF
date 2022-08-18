1ab7e950f1b3b137e3983743363672752f180831
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-08-18 18:33:30 +0500

    add(Entities): add models class lib

---
+++ Entities/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Genre
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int GenreId { get; set; }
+
+        [Required(ErrorMessage = "Genre name is required")]
+        [StringLength(10, ErrorMessage = "Genre name can't be longer than 10 characters")]
+        public string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ Entities/Models/MovieWorker.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class MovieWorker
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Worker fullname is required")]
+        [StringLength(30, ErrorMessage = "Name can't be longer than 30 characters")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Worker picture url is required")]
+        public string PictureUrl { get; set; }
+
+        public ICollection<Movie> ActedMovies { get; set; }
+
+        public ICollection<Movie> DirectedMovies { get; set; }
+    }
+}
