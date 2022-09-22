ee86542d198da5d11e301e28e227eb95ec954a3a
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-09-23 04:11:30 +0500

    test(Controllers): add unit test for MovieController GetAllAsync method

---
+++ MovieRestApiWithEF.Tests/Controllers/MoviesControllerTests/GetAllAsyncTest.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Contracts;
+using Entities.Models;
+using Entities.Responses;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using MovieRestApiWithEF.Controllers;
+using System.Collections;
+
+namespace MovieRestApiWithEF.Tests.Unit.Controllers.MoviesControllerTests
+{
+    public class GetAllAsyncTest
+    {
+        private readonly Mock<IRepositoryManager> _mockRepositoryManager;
+        private readonly ILoggerManager _stubbedLogger;
+        private readonly IMapper _mapper;
+
+        public GetAllAsyncTest()
+        {
+            _mockRepositoryManager = new Mock<IRepositoryManager>();
+            _stubbedLogger = new Mock<ILoggerManager>().Object;
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            _mapper = new Mapper(configuration);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_FindsMovies_Returns200Status()
+        {
+            /// Arrange
+
+            // Prepare MovieRepository mock
+            var mockMovieRepo = new Mock<IMovieRepository>();
+            mockMovieRepo.Setup(x => x.FindAllAsync()).ReturnsAsync(MoviesMockData.GetMovies());
+
+            // Prepare IoC container mock
+            _mockRepositoryManager.Setup(x => x.MovieRepository).Returns(mockMovieRepo.Object);
+
+            // Prepare System under test (sut)
+            var sut = new MoviesController(_mockRepositoryManager.Object, _stubbedLogger, _mapper);
+
+            /// Act
+            var result = await sut.GetAllAsync();
+
+            /// Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+            ((OkObjectResult)result).StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_FindsMovies_ReturnsMoviesWithDetails()
+        {
+            /// Arrange
+
+            // Prepare MovieRepository mock
+            var mockMovieRepo = new Mock<IMovieRepository>();
+            mockMovieRepo.Setup(x => x.FindAllAsync()).ReturnsAsync(MoviesMockData.GetMovies());
+
+            // Prepare IoC container mock
+            _mockRepositoryManager.Setup(x => x.MovieRepository).Returns(mockMovieRepo.Object);
+
+            // Prepare System under test (sut)
+            var sut = new MoviesController(_mockRepositoryManager.Object, _stubbedLogger, _mapper);
+
+            /// Act
+            var okResult = await sut.GetAllAsync() as OkObjectResult;
+
+            /// Assert
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().BeEquivalentTo(MoviesMockData.GetMoviesResponse());
+        }
+
+        [Fact]
+        public async Task GetAllAsync_NoMovies_Returns200Status()
+        {
+            /// Arrange
+
+            // Prepare MovieRepository mock
+            var mockMovieRepo = new Mock<IMovieRepository>();
+            mockMovieRepo.Setup(x => x.FindAllAsync()).ReturnsAsync(MoviesMockData.GetEmptyMovies());
+
+            // Prepare IoC container mock
+            _mockRepositoryManager.Setup(x => x.MovieRepository).Returns(mockMovieRepo.Object);
+
+            // Prepare System under test (sut)
+            var sut = new MoviesController(_mockRepositoryManager.Object, _stubbedLogger, _mapper);
+
+            /// Act
+            var result = await sut.GetAllAsync();
+
+            /// Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+            ((OkObjectResult)result).StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_NoMovies_ReturnsEmptyMovies()
+        {
+            /// Arrange
+
+            // Prepare MovieRepository mock
+            var mockMovieRepo = new Mock<IMovieRepository>();
+            mockMovieRepo.Setup(x => x.FindAllAsync()).ReturnsAsync(MoviesMockData.GetEmptyMovies());
+
+            // Prepare IoC container mock
+            _mockRepositoryManager.Setup(x => x.MovieRepository).Returns(mockMovieRepo.Object);
+
+            // Prepare System under test (sut)
+            var sut = new MoviesController(_mockRepositoryManager.Object, _stubbedLogger, _mapper);
+
+            /// Act
+            var okResult = await sut.GetAllAsync() as OkObjectResult;
+
+            /// Assert
+            okResult.Should().NotBeNull();
+            okResult!.Value.Should().BeEquivalentTo(MoviesMockData.GetEmptyMoviesResponse());
+        }
+    }
+}
---
+++ MovieRestApiWithEF.Tests/Controllers/MoviesControllerTests/MoviesMockData.cs
@@ -0,0 +1,143 @@
+﻿using Entities.Models;
+using Entities.Responses;
+
+namespace MovieRestApiWithEF.Tests.Unit.Controllers.MoviesControllerTests
+{
+    public class MoviesMockData
+    {
+        private static DateTime DateTime = DateTime.Now;
+
+        public static List<Movie> GetMovies()
+        {
+            return new List<Movie>{
+                 new Movie{
+                     Id = 1,
+                     Title = "Movie 1",
+                     ReleaseDate = DateTime,
+                     DirectorId = 1,
+                     GenreId = 1,
+                     Director = new MovieWorker
+                     {
+                        Id = 1,
+                        FullName = "Director 1",
+                        PictureUrl = "some-url",
+                     },
+                     Genre = new Genre
+                     {
+                         Id = 1,
+                         Name = "Genre 1",
+                     },
+                     Cast = new List<MovieWorker>(),
+                 },
+                 new Movie{
+                     Id = 2,
+                     Title = "Movie 2",
+                     ReleaseDate = DateTime,
+                     DirectorId = 2,
+                     GenreId = 2,
+                     Director = new MovieWorker
+                     {
+                        Id = 2,
+                        FullName = "Director 2",
+                        PictureUrl = "some-url",
+                     },
+                     Genre = new Genre
+                     {
+                         Id = 2,
+                         Name = "Genre 2",
+                     },
+                     Cast = new List<MovieWorker>(),
+                 },
+                 new Movie{
+                     Id = 3,
+                     Title = "Movie 3",
+                     ReleaseDate = DateTime,
+                     DirectorId = 3,
+                     GenreId = 3,
+                     Director = new MovieWorker
+                     {
+                        Id = 3,
+                        FullName = "Director 3",
+                        PictureUrl = "some-url",
+                     },
+                     Genre = new Genre
+                     {
+                         Id = 3,
+                         Name = "Genre 3",
+                     },
+                     Cast = new List<MovieWorker>(),
+                 },
+            };
+        }
+
+        public static List<MovieWithDetailsResponse> GetMoviesResponse()
+        {
+            return new List<MovieWithDetailsResponse>{
+                 new MovieWithDetailsResponse
+                 {
+                     Id = 1,
+                     Title = "Movie 1",
+                     ReleaseDate = DateTime,
+                     Director = new MovieWorkerResponse
+                     {
+                        Id = 1,
+                        FullName = "Director 1",
+                        PictureUrl = "some-url"
+                     },
+                     Genre = new GenreResponse
+                     {
+                         Id = 1,
+                         Name = "Genre 1",
+                     },
+                     Cast = new List<MovieWorkerResponse>(),
+                 },
+                 new MovieWithDetailsResponse
+                 {
+                     Id = 2,
+                     Title = "Movie 2",
+                     ReleaseDate = DateTime,
+                     Director = new MovieWorkerResponse
+                     {
+                        Id = 2,
+                        FullName = "Director 2",
+                        PictureUrl = "some-url"
+                     },
+                     Genre = new GenreResponse
+                     {
+                         Id = 2,
+                         Name = "Genre 2",
+                     },
+                     Cast = new List<MovieWorkerResponse>(),
+                 },
+                 new MovieWithDetailsResponse
+                 {
+                     Id = 3,
+                     Title = "Movie 3",
+                     ReleaseDate = DateTime,
+                     Director = new MovieWorkerResponse
+                     {
+                        Id = 3,
+                        FullName = "Director 3",
+                        PictureUrl = "some-url"
+                     },
+                     Genre = new GenreResponse
+                     {
+                         Id = 3,
+                         Name = "Genre 3",
+                     },
+                     Cast = new List<MovieWorkerResponse>(),
+                 },
+            };
+        }
+
+        public static List<Movie> GetEmptyMovies()
+        {
+            return new List<Movie>();
+        }
+
+        public static List<MovieResponse> GetEmptyMoviesResponse()
+        {
+            return new List<MovieResponse>();
+        }
+    }
+}
