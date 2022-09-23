3cfa3c09933149e77924adc8f5192d9c6030287d
Abdur Rafay Saleem <a.rafaysaleem@gmail.com>
2022-09-24 04:22:35 +0500

    test(Helpers): add test helpers

---
+++ MovieRestApiWithEF.Tests/Helpers/TestHelpers.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace MovieRestApiWithEF.Tests.Unit.Helpers
+{
+    public class TestHelpers
+    {
+        public static IMapper GetTestAutoMapper()
+        {
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(new MappingProfile()));
+            return new Mapper(configuration);
+        }
+    }
+}
